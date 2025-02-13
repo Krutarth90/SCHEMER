d0480af92a4b9e4a7520ff7aab408a3191a893e5
krutarth <krutarthpipaliya90@gmail.com>
2025-02-14 00:14:06 +0530

    Removed node_modules and dist from tracking

--- BE/dist/routes/middlewares/tokenValidate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenValidate = tokenValidate;
-function tokenValidate(req, res, next) {
-    if (!localStorage.getItem("token")) {
-    }
-}
